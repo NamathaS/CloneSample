c2de05c4dbadc45e6647648bd9c7f50c7ef97405
NamathaS <namathascavish@hotmail.com>
2020-11-23 13:45:32 +1300

    new changes

--- src/basics/SampleProgram.java
+++ src/basics/SampleProgram.java
@@ -4,7 +4,7 @@ public class SampleProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Sample Program");
+		System.out.println("Sample Java Hello World");
 	}
 
 }
